c9bacc6ba9cf43783d94e1de118f940bf62020aa
Mariza Pereira <marupereira@ladmins-MacBook.local>
2016-10-03 16:32:25 -0700

    First commit

---
+++ grades.py
@@ -0,0 +1,20 @@
+def compute_grade():
+	try:
+	    score = raw_input('What was the students score, between 0.0  - 1.1? ')
+	    s = float(score)
+	    if s >= 0.9 and s <= 1.1:
+		    grade = 'A'
+	    elif s >= 0.8 and s < 0.9:
+		    grade = 'B'
+	    elif s >= 0.7 and s < 0.8:
+		    grade = 'C'
+	    elif s >= 0.6 and s < 0.7:
+		    grade = 'D'
+	    elif s <= 0.6:
+		    grade = 'F'
+	    return grade
+	except: 
+		print "Bad score. Please try again with a score between 0.1-1.0"
+	
+	
+print "You final grade is: " + compute_grade()
